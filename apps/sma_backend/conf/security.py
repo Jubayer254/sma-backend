863569d431f9623373b45c32cc25a3dc74eb008b
Jubayer Hossen <jubayerhossen254@example.com>
2025-07-13 02:04:16 +0600

    Allowed Host Added

--- apps/sma_backend/conf/security.py
+++ apps/sma_backend/conf/security.py
@@ -4,7 +4,7 @@ SECRET_KEY = "django-insecure-7t3k@x^plf-z$t3#p+ze_o@u^4ydkk%z=37rx$-5ms)hd3c(!v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '16.176.7.48']
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
