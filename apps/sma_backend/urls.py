226cd9c9c970dc59a1b0b420c83e97aa90e6aed5
Jubayer Hossen <jubayerhossen254@example.com>
2025-07-20 19:55:57 +0600

    Course Added

--- apps/sma_backend/urls.py
+++ apps/sma_backend/urls.py
@@ -17,11 +17,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', lambda request: redirect('admin/', permanent=False)),
     path('auth/', include('djoser.urls')),  # User routes
     path('auth/', include('djoser.urls.jwt')),  # JWT endpoints
     path('api/', include('course_material.urls')),  # Course material API
